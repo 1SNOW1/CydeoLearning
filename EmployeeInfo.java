28dc3bac87ad2d098f8eca6d313d7f193d01491b
1SNOW1 <60426461+1SNOW1@users.noreply.github.com>
2023-11-15 06:12:47 +0100

    Add files via upload

---
+++ EmployeeInfo.java
@@ -0,0 +1,31 @@
+package day02_escapeSequences;
+
+import java.sql.SQLOutput;
+
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Name: Enes Orhan");
+        System.out.println("Gender: Male");
+        System.out.println("Age: 21");
+        System.out.println("Job Title: Java Developer");
+        System.out.println("Company: Blizzard");
+        System.out.println("Salary: 200 000$");
+        /* this is a multi line comment
+        and this is written to make sure the multi line comment works
+        and this is to secure the thesis,
+        thank you for your understanding
+        dear reader
+         */
+    }
+}
+
+
+
+  /*
+                1. \n : New line
+                2. \t : paragraph space
+                3. \\ : back slash "\"
+                4. \" : double quotes "
+         */
