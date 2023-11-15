c5a67bbd184f6e8866aa5f65679e834b50c34912
Enesorhan <enestcp@gmail.com>
2023-11-15 05:14:31 +0100

    commit

---
+++ src/day01_introToProgramming/helloCydeo.java
@@ -0,0 +1,11 @@
+package day01_introToProgramming;
+
+public class helloCydeo {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello Cydeo");
+        System.out.println("How are you?");
+    }
+
+}
---
+++ src/day3_variables/EscapeSequencePractice.java
@@ -0,0 +1,9 @@
+package day3_variables;
+
+public class EscapeSequencePractice {
+
+    public static void main(String[] args) {
+
+        System.out.println("My favorite programming language is Java \n Java is an \"Object-Oriented\" programming language");
+    }
+}
