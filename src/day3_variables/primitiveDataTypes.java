2a4d6f8d85a44e759244e8ce5c2a2f9f9332f688
Enesorhan <enestcp@gmail.com>
2023-11-17 00:46:35 +0100

    commit

---
+++ src/day3_variables/primitiveDataTypes.java
@@ -0,0 +1,37 @@
+package day3_variables;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class primitiveDataTypes {
+
+    public static void main(String[] args) {
+
+
+        // DataType variableName = Data;
+
+        // byte a = "Java"; // byte ONLY accepts integer values (-128 - 127)
+
+        // byte a = 20.5; // byte can not take decimal numbers only integers
+        //byte a = 2000; // out of bytes range
+
+        byte a = 20;
+
+        //price of the is $17500
+
+        short p = 17500;
+
+        System.out.println("p");   // will only print out p
+        System.out.println(p);  // will print out the value assigned to p
+
+        //salary is $95000
+
+        int s = 95000;
+
+        System.out.println(s);
+
+    }
+
+
+}
