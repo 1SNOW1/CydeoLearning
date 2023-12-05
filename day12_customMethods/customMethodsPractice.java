e93948c5a1fad95837c115166cbb61b0c61d30e1
enesorhan <enestcp@gmail.com>
2023-12-05 18:43:38 +0100

    main

--- day12_customMethods/customMethodsPractice.java
+++ day12_customMethods/customMethodsPractice.java
@@ -1,24 +1,42 @@
 package day12_customMethods;
 
+import javax.xml.validation.ValidatorHandler;
+
 public class customMethodsPractice {
 
     public static void main(String[] args) {
 
-        maxNum(100, 2000);
+        maxNum(100, 100);
 
+
+        System.out.println("--------------------");
+
+
+        initial("Enes", "Orhan");
     }
 
 
     public static void maxNum(double num1, double num2){
 
         if(num1 > num2){
-            System.out.println("Maximum number is " + num1);
+            System.out.println("Biggest number is " + num1);
         }else if(num2 > num1){
-            System.out.println("Maximum number is " + num2);
+            System.out.println("Biggest number is " + num2);
         }else {
             System.out.println("Equal");
         }
 
 
     }
+
+//create a method that can display the initials of a person (firstname, lastname)
+
+    public static void initial(String firstName, String lastName ){
+
+        String initial = firstName.charAt(0) + "," + lastName.charAt(0);
+
+        System.out.println("Initial is: " + initial);
+
+    }
+
 }
---
+++ day12_customMethods/test.java
@@ -0,0 +1,15 @@
+package day12_customMethods;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        customMethodsWithParameters.oddOrEven(333);
+
+
+        String firstName = "Vasyl",
+                lastName = "Dobrianski";
+
+        customMethodsPractice.initial(firstName, lastName);
+    }
+}
