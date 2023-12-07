07c7139a3b26cb8d33e12d58809c26dbc725619a
enesorhan <enestcp@gmail.com>
2023-12-07 11:46:40 +0100

    main

---
+++ day13_customMethods/customMethods3.java
@@ -0,0 +1,32 @@
+package day13_customMethods;
+
+public class customMethods3 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static void calculate(double num1, double num2, double result, char mathOperator){
+
+
+        double result = 0;
+
+        switch (mathOperator){
+
+            case '+': result = num1 + num2;
+                System.out.println(num1 +" "+mathOperator+" "+num2+" "+ " = "+ result);
+            case '-': result = num1 - num2;
+                System.out.println(num1 +" "+mathOperator+" "+num2+" "+ " = "+ result);
+            case '*': result = num1 * num2;
+                System.out.println(num1 +" "+mathOperator+" "+num2+" "+ " = "+ result);
+            case '/': result = num1 / num2;
+                System.out.println(num1 +" "+mathOperator+" "+num2+" "+ " = "+ result);
+
+            default:
+        }
+
+    }
+
+}
