3753455be0e15a759fae896b4b46990bc4e43c83
enesorhan <enestcp@gmail.com>
2023-12-15 07:22:39 +0100

    main

---
+++ day16_nestedLoop/nestedLoop1.java
@@ -0,0 +1,23 @@
+package day16_nestedLoop;
+
+public class nestedLoop1 {
+
+    public static void main(String[] args) {
+
+        /*
+        This loop below is basically, whenever the outer loop runs one time the nested loop will run 5 times.
+        At the end we will see 25 Wooden Spoons.
+         */
+        for (int i = 0; i < 5 ; i++) {
+
+            for (int j = 0; j < 5; j++) {
+                System.out.println("Wooden Spoon");
+
+            }
+
+
+
+
+        }
+    }
+}
