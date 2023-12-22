653abd37e43d00fb4043af6195b0870973f4d217
enesorhan <enestcp@gmail.com>
2023-12-22 21:38:50 +0100

    main

---
+++ day19_array/ArraysPractice1.java
@@ -0,0 +1,26 @@
+package day19_array;
+
+public class ArraysPractice1 {
+
+    public static void main(String[] args) {
+
+        String[] myGroup = new String[5];
+        myGroup[0] = "Ömer";
+        myGroup[1] = "Eyyup";
+        myGroup[2] = "Ay evelyn";
+        myGroup[3] = "Art hoxa";
+        myGroup[4] = "Cundullah";
+
+        for (int i = 0; i < myGroup.length ; i++) {
+            System.out.println(myGroup[i]);
+        }
+
+        System.out.println("-----------------------------------");
+
+        //what if I want to display the names in reverse order, meaning from last to first one in myGroup
+        for (int i = myGroup.length-1; i >= 0 ; i--) {
+            System.out.println(myGroup[i]);
+            
+        }
+    }
+}
