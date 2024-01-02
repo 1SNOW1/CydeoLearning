1ca5ccdd6fc7d0e491cecde6a253cb6fe9af07de
Enesorhan <enestcp@gmail.com>
2024-01-02 16:19:02 +0100

    latest changes

---
+++ src/day24_dateAndTime/Person.java
@@ -0,0 +1,30 @@
+package day24_dateAndTime;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    public String name;
+    public int age;
+    public char gender;
+    public LocalDate dateOfBirth;
+
+    public void setInfo(String name, char gender, LocalDate dateOfBirth) {
+        this.name = name;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        age = LocalDate.now().getYear() - dateOfBirth.getYear();
+        //I don't need to use this.age on the age declaration because
+        //I don't have a local var called age, so age only is sufficient
+     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+}
