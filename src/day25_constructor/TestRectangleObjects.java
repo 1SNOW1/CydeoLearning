283c54cdad9f701af1c9dcb4bf6fb0d72ea8c9db
Enesorhan <enestcp@gmail.com>
2024-01-04 18:27:26 +0100

    latest changes

---
+++ src/day25_constructor/TestRectangleObjects.java
@@ -0,0 +1,61 @@
+package day25_constructor;
+
+public class TestRectangleObjects {
+
+
+    public static void main(String[] args) {
+
+
+
+        /*
+        Rectangle rectangle1 = new Rectangle();
+        rectangle1.setInfo(3,4);
+
+        Rectangle rectangle2 = new Rectangle();
+
+        Rectangle rectangle3 = new Rectangle();
+
+        Rectangle rectangle4 = new Rectangle();
+
+        Rectangle rectangle5 = new Rectangle();
+
+
+         */
+
+
+        Rectangle rectangle1 = new Rectangle(6,6);
+        Rectangle rectangle2 = new Rectangle(6,6);
+        Rectangle rectangle3 = new Rectangle(6,6);
+        Rectangle rectangle4 = new Rectangle(6,6);
+        Rectangle rectangle5 = new Rectangle(6,6);
+        Rectangle rectangle6 = new Rectangle(6,6);
+
+
+
+
+        System.out.println(rectangle1);
+        System.out.println(rectangle2);
+        System.out.println(rectangle3);
+        System.out.println(rectangle4);
+        System.out.println(rectangle5);
+        System.out.println(rectangle6);
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
